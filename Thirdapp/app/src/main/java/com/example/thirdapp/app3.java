a39525f1c104468b7824885c6381ddd21253d9cc
prasad52512 <35742658+prasad52512@users.noreply.github.com>
2019-06-02 21:17:05 +0530

    Fourthapp

---
+++ Thirdapp/app/src/main/java/com/example/thirdapp/app3.java
@@ -0,0 +1,13 @@
+package com.example.thirdapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class app3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app3);
+    }
+}

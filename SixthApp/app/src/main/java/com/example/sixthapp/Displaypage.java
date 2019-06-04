2a321ec41a189c2bfda91987aac21360c9eae694
prasad52512 <35742658+prasad52512@users.noreply.github.com>
2019-06-04 23:29:20 +0530

    SixthApp-LoginPage

---
+++ SixthApp/app/src/main/java/com/example/sixthapp/Displaypage.java
@@ -0,0 +1,13 @@
+package com.example.sixthapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Displaypage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_displaypage);
+    }
+}

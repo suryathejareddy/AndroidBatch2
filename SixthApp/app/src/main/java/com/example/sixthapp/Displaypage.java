4fa69a2d54bcfe00d6e02f89a8658a4a80279064
prasad52512 <35742658+prasad52512@users.noreply.github.com>
2019-06-12 18:06:11 +0530

    SixthApp

--- SixthApp/app/src/main/java/com/example/sixthapp/Displaypage.java
+++ SixthApp/app/src/main/java/com/example/sixthapp/Displaypage.java
@@ -2,12 +2,37 @@ package com.example.sixthapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Displaypage extends AppCompatActivity {
-
+    TextView t1;
+    TextView t2;
+    TextView t3;
+    TextView t4;
+    TextView t5;
+    String name,rollno,department,contactno,email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_displaypage);
+        t1=findViewById(R.id.tvname);
+        t2=findViewById(R.id.tvrollno);
+        t3=findViewById(R.id.tvdepartment);
+        t4=findViewById(R.id.tvcontactno);
+        t5=findViewById(R.id.tvemail);
+        Bundle b=getIntent().getExtras();
+        name=b.get("name").toString();
+        t1.setText(name);
+        name=b.get("rollno").toString();
+        t2.setText(rollno);
+        name=b.get("department").toString();
+        t3.setText(department);
+        name=b.get("contactno").toString();
+        t4.setText(contactno);
+        name=b.get("email").toString();
+        t5.setText(email);
+
+
     }
 }
--- SixthApp/app/src/main/java/com/example/sixthapp/MainActivity.java
+++ SixthApp/app/src/main/java/com/example/sixthapp/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.sixthapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    String name,rollno,department,contactno,email,username,password;
+    EditText e1;
+    EditText e2;
+    EditText e3;
+    EditText e4;
+    EditText e5;
+    EditText e6;
+    EditText e7;
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        e1=findViewById(R.id.etname);
+        e2=findViewById(R.id.etrollno);
+        e3=findViewById(R.id.etdepartment);
+        e4=findViewById(R.id.etcontactno);
+        e5=findViewById(R.id.etmail);
+        e6=findViewById(R.id.etusername);
+        e7=findViewById(R.id.etpassword);
+        b=findViewById(R.id.bt);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name=e1.getText().toString();
+                rollno=e2.getText().toString();
+                department=e3.getText().toString();
+                contactno=e4.getText().toString();
+                email=e5.getText().toString();
+                username=e6.getText().toString();
+                password=e7.getText().toString();
+
+                Intent i=new Intent(MainActivity.this,Displaypage.class);
+                i.putExtra("name",name);
+                i.putExtra("rollno",rollno);
+                i.putExtra("department",department);
+                i.putExtra("contactno",contactno);
+                i.putExtra("email",email);
+                i.putExtra("username",username);
+                i.putExtra("password",password);
+startActivity(i);
+            }
+        });
     }
 }

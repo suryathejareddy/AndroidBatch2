4a3cffe6251499089fc1fb16ff5f2260578e283c
kaushikavinash <51161194+kaushikavinash@users.noreply.github.com>
2019-06-14 10:38:55 +0530

    kaushik

--- loginID/app/src/main/java/com/example/loginid/Main2Activity.java
+++ loginID/app/src/main/java/com/example/loginid/Main2Activity.java
@@ -1,22 +1,22 @@
-package com.example.kaushik;
+package com.example.loginid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class Main5Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
     TextView t1,t2,t3,t4,t5,t6;
     String name,rollno,age,email,username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main5);
+        setContentView(R.layout.activity_main2);
         t1=(TextView)findViewById(R.id.etname);
         t2=(TextView)findViewById(R.id.etrollno);
         t3=(TextView)findViewById(R.id.etage);
         t4=(TextView)findViewById(R.id.etemailid);
         t5=(TextView)findViewById(R.id.etusername);
-t6=(TextView)findViewById(R.id.etpassword);
+        t6=(TextView)findViewById(R.id.etpassword);
     }
 }
--- loginID/app/src/main/java/com/example/loginid/MainActivity.java
+++ loginID/app/src/main/java/com/example/loginid/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.kaushik;
+package com.example.loginid;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Main2Activity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     String name ,rollno,age,emailid,username,password;
     EditText e1;
     EditText e2;
@@ -31,18 +31,18 @@ public class Main2Activity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               name=e1.getText().toString();
-               rollno=e2.getText().toString();
-               age=e3.getText().toString();
-               emailid=e4.getText().toString();
-               username=e5.getText().toString();
-               password=e6.getText().toString();
-                Intent i=new Intent(Main2Activity.this,Main5Activity.class);
+                name=e1.getText().toString();
+                rollno=e2.getText().toString();
+                age=e3.getText().toString();
+                emailid=e4.getText().toString();
+                username=e5.getText().toString();
+                password=e6.getText().toString();
+                Intent i=new Intent(MainActivity.this,Main2Activity.class);
                 i.putExtra("name",name);
                 i.putExtra("rollno",rollno);
                 i.putExtra("age",age);
                 i.putExtra("emailid",emailid);
-               i.putExtra("password",password);
+                i.putExtra("password",password);
 
 
             }

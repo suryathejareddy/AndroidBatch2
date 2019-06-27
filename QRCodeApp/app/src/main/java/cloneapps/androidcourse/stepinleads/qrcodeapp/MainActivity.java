6250a5b75ca63445906e1ee25bee66dc4595acfa
suryathejareddy <surya.seggari@gmail.com>
2019-06-27 17:41:10 +0530

    QRCodeApp

---
+++ QRCodeApp/app/src/main/java/cloneapps/androidcourse/stepinleads/qrcodeapp/MainActivity.java
@@ -0,0 +1,32 @@
+package cloneapps.androidcourse.stepinleads.qrcodeapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button gen, scan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gen = (Button) findViewById(R.id.gen);
+        scan = (Button) findViewById(R.id.scan);
+        gen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gIntent = new Intent(MainActivity.this, Generator.class);
+                startActivity(gIntent);
+            }
+        });
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent rIntent = new Intent(MainActivity.this, Reader.class);
+                startActivity(rIntent);
+            }
+        });
+    }
+}
